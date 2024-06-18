517a4873afeb4458906ca4e00ae268ff6a112cf9
manasa y <123yapitsme@gmail.com>
2024-06-18 07:06:06 +0530

    navbar is written

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>tadaaaaaa</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
